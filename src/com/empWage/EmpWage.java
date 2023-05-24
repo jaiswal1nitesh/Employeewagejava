63c166125e710de7d44c871e680748922c8fac42
jaiswal1nitesh <nj639476@gmail.comgit config --global user.email nj639476@gmail.com>
2023-05-24 14:17:43 +0530

    Added Welcome to employee wage problem

---
+++ src/com/empWage/EmpWage.java
@@ -0,0 +1,10 @@
+package com.empWage;
+
+public class EmpWage {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee \n" +
+                "Wage Computation \n" +
+                "Program");
+    }
+
+}
