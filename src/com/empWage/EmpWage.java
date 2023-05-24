7f90564375397a15e7a28fcee462d061e16ba5c0
jaiswal1nitesh <nj639476@gmail.comgit config --global user.email nj639476@gmail.com>
2023-05-24 14:30:54 +0530

    Added Employee Attendance

--- src/com/empWage/EmpWage.java
+++ src/com/empWage/EmpWage.java
@@ -1,10 +1,26 @@
 package com.empWage;
 
+import java.util.Random;
+
 public class EmpWage {
+    static final int empPresent = 1;
+    static final int empAbsent = 0;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Employee \n" +
                 "Wage Computation \n" +
                 "Program");
+
+
+        Random rand = new Random();
+        int num = rand.nextInt(0, 2);
+
+        if (num == empAbsent)
+            System.out.println("Employee is Absent");
+        else
+            System.out.println("Employee is Present");
+
+
     }
 
 }
