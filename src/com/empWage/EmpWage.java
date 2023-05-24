828c737dedbb7b6299b66242933f08fbab8b317e
jaiswal1nitesh <nj639476@gmail.comgit config --global user.email nj639476@gmail.com>
2023-05-24 14:50:37 +0530

    Added Calculate Employee wage

--- src/com/empWage/EmpWage.java
+++ src/com/empWage/EmpWage.java
@@ -5,20 +5,28 @@ import java.util.Random;
 public class EmpWage {
     static final int empPresent = 1;
     static final int empAbsent = 0;
+    static int empWagePerHours = 20;
+    static int empTotalWage;
+
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee \n" +
-                "Wage Computation \n" +
-                "Program");
+        System.out.println("Welcome to Employee wage Problem");
 
 
         Random rand = new Random();
         int num = rand.nextInt(0, 2);
 
-        if (num == empAbsent)
+        if (num == empAbsent) {
             System.out.println("Employee is Absent");
-        else
+            int empWorkingPerHours = 0;
+            empTotalWage = empWorkingPerHours * empWagePerHours;
+
+        } else {
+            int empWorkingPerHours = 8;
             System.out.println("Employee is Present");
+            empTotalWage = empWorkingPerHours * empWagePerHours;
+        }
+        System.out.println(empTotalWage);
 
 
     }
